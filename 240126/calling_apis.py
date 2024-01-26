a2ef3daba22cb87dc0c5eeb0e05f72d5133b663e
Maaike <maaikejvp@hotmail.com>
2024-01-26 11:39:00 +0100

    post put delete

--- 240126/calling_apis.py
+++ 240126/calling_apis.py
@@ -3,6 +3,7 @@ import json
 
 base_url = "https://jsonplaceholder.typicode.com/"
 
+# get example
 response = requests.get(base_url + "users")
 
 print(response)
@@ -14,3 +15,32 @@ if response.status_code == 200:
         print(user["name"], user["address"]["city"])
 else:
     print("The call failed: ", response.status_code)
+
+# post example
+comment_obj = {
+    "name": "The header",
+    "body": "blablabla",
+    "email": "bla@bla.bla",
+    "postId": 2
+}
+
+response2 = requests.post(base_url + "comments", json.dumps(comment_obj))
+print(response2.status_code)
+print(response2.json())
+
+
+# delete example
+response3 = requests.delete(base_url + "comments/65")
+print(response3.status_code)
+print(response3.json())
+
+comment_obj2 = {
+    "name": "The new header",
+    "body": "new blablabla",
+    "email": "bla@bla.bla",
+    "postId": 2
+}
+
+response4 = requests.put(base_url + "comments/2", json.dumps(comment_obj2))
+print(response4.status_code)
+print(response4.json())
--- 240126/exercise-requests.md
+++ 240126/exercise-requests.md
@@ -5,4 +5,10 @@ Call the JSON placeholder API and get all the comments. Print the name of the co
 If a call was not successful, print an error message.
 
 ## Exercise 2
-Find a nice open API that doesn't require a key. Call the API using Python and process the result (example in `joke_api.py`)
+Find a nice open API that doesn't require a key. Call the API using Python and process the result (example in `joke_api.py`). You can find fun one on apipheny.com
+
+## Exercise 3
+Call the JSON placeholder API and do the following:
+- Post a new album (/albums)
+- Update a comment (/comments)
+- Delete a post (/posts)
